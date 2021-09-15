1ad5e687f790b19b6686e0885bdf3dbd0575e45a
Jeongseup <thswjdtmq4@inu.ac.kr>
2021-09-15 10:31:29 +0900

    [UPDATE] ERC721 배포 테스트

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,9 +1,23 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+
+require("dotenv").config();
+const { API_URL, PRIVATE_KEY } = process.env;
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
+// console.log(API_URL, PRIVATE_KEY);
 
 module.exports = {
-  solidity: "0.7.3",
+    solidity: "0.8.0",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {},
+        ropsten: {
+            url: API_URL,
+            accounts: [`0x${PRIVATE_KEY}`],
+        },
+    },
 };
---
+++ hardhat/test.js
@@ -0,0 +1,4 @@
+const dotenv = require("dotenv").config();
+// const { API_URL, PRIVATE_KEY } = process.env;
+
+console.log(dotenv);
