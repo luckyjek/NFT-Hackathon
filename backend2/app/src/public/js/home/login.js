a11fc7c64376a117dc6df9538daf64006d241e44
Jeongseup <thswjdtmq4@inu.ac.kr>
2021-09-19 16:37:29 +0900

    DOM으로 객체 제어하기, 5:36

--- backend2/app/src/public/js/home/login.js
+++ backend2/app/src/public/js/home/login.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log("hello");
+const id = document.querySelector("#id"),
+    pw = document.querySelector("#pw"),
+    loginBtn = document.querySelector("button");
+
+// console.log(id, pw, loginBtn);
+loginBtn.addEventListener("click", login);
+
+function login() {
+    console.log("login!");
+    console.log(id.value);
+    console.log(pw.value);
+}
