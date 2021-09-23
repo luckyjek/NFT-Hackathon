929aaf704fa8c2ae015415cc55fe013794161f41
EUNKYUNG JUNG <thxjek@gmail.com>
2021-09-23 20:40:32 +0900

    Add favicon&update_profile.js

--- client/js/artInfo.js
+++ client/js/artInfo.js
@@ -9,34 +9,35 @@ for (let i = 0; i < data.length; i++) {
     artDate = data[i].art_update;
     artPrice = data[i].art_price;
 
-    artInfoList += ` <div class="left">
-  <div class="artInfo__cardImg">
-    <img class="artInfo__img" src="${artImg}" />
-  </div>
-  <div class="artInfo__textBox">
-    <div class="artInfo__top">
-      <div class="artInfo__title">
-        <h2 class="nftText__title">${artName}</h2>
-        <h3 class="nftText__number">${nftNum}</h3>
-        <h6 class="nftText__artist">Created by ${artistName}</h6>
+    artInfoList += ` 
+    <div class="left">
+      <div class="artInfo__cardImg">
+        <img class="artInfo__img" src="${artImg}" />
       </div>
-      <div class="artInfo__price">
-        <span class="nftText__price"
-          ><i class="fab fa-ethereum fa-sm"></i>${artPrice}</span
-        >
+      <div class="artInfo__textBox">
+      <div class="artInfo__top">
+        <div class="artInfo__title">
+          <h2 class="nftText__title">${artName}</h2>
+          <h3 class="nftText__number">${nftNum}</h3>
+          <h6 class="nftText__artist">Created by ${artistName}</h6>
+        </div>
+        <div class="artInfo__price">
+          <span class="nftText__price"
+            ><i class="fab fa-ethereum fa-sm"></i>${artPrice}</span
+          >
+        </div>
       </div>
-    </div>
-    <div class="artInfo__description">
-      <h5>작품 설명</h5>
-      <p class="nftText__description">
-      ${artInfo}
-      </p>
-    </div>
-    <div class="artInfo__btn">
-      <button class="nftText__btn" onclick="artInfo__donateBtn()">
-        Donate
-      </button>
-    </div>
+      <div class="artInfo__description">
+        <h5>작품 설명</h5>
+        <p class="nftText__description">
+        ${artInfo}
+        </p>
+      </div>
+      <div class="artInfo__btn">
+        <button class="nftText__btn" onclick="artInfo__donateBtn()">
+          Donate
+        </button>
+      </div>
   </div>
 </div>
   `;
--- client/js/profile.js
+++ client/js/profile.js
@@ -32,3 +32,39 @@ function removeF() {
 //     console.log(response.data);
 //     console.log(response.data.Userdata[0].user_nickname);
 // });
+
+var UserInfo = [
+    {
+        user_id: "0",
+        artist_name: "eunkyung",
+        wallet_addr: "0x602289e292739x39372s",
+        user_email: "ek@gmail.com",
+    },
+    {
+        user_id: "1",
+        artist_name: "heekyung",
+        wallet_addr: "0x60239e292739x3922312s",
+        user_email: "hk@gmail.com",
+    },
+    {
+        user_id: "2",
+        artist_name: "jangyeon",
+        wallet_addr: "0x642289e291739x246572s",
+        user_email: "jy@gmail.com",
+    },
+    {
+        user_id: "3",
+        artist_name: "jeongseup",
+        wallet_addr: "0x602283e292e7539x39372s",
+        user_email: "js@gmail.com",
+    },
+];
+
+console.log(UserInfo[0].user_id);
+console.log(UserInfo[0].artist_name);
+console.log(UserInfo[0].wallet_addr);
+console.log(UserInfo[0].user_email);
+
+document.querySelector("#artist_name").innerHTML = UserInfo[0].artist_name;
+document.querySelector("#wallet_addr").innerHTML = UserInfo[0].wallet_addr;
+document.querySelector("#user_email").innerHTML = UserInfo[0].user_email;
